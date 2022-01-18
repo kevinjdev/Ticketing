a57cb14d8cba57355c7ab0cf929621eca3729fcf
kjaeger <kevin.a.jaeger@gmail.com>
2022-01-18 15:10:01 -0700

    deploy

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ const buildClient = ({ req }) => {
 
     return axios.create({
       baseURL:
-        //  'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+        ///'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
         'http://www.kevinjdev.xyz/',
       headers: req.headers,
     });
